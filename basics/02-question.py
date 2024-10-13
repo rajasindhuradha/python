e2e7a63f8a18db608d799595204c27c2bea40e04
Raja Manikandan <manikandanradha15@gmail.com>
2024-10-13 18:11:42 +0530

    Added some python files

---
+++ basics/02-question.py
@@ -0,0 +1,8 @@
+name=input("Enter a name: ")
+print(type(name))
+score=int(input("Enter a number: "))
+print(type(score))
+department=input("Enter a department: ")
+print("My name is:",name)
+print("My score is:",score/10,"/10")
+print("My department is:",department)
--- basics/03-casting.py
+++ basics/03-casting.py
@@ -1,4 +1,8 @@
 name="EMC"
 age=1
+print(type(name))
+print(type(age))
+age=str(age)
+print(type(age))
 print("My name is",name)
 print("My age is",age)
